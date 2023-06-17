51f39272a727ec8a6284682f37ab1a99b99c8816
RebeccaKen <kennedyrebecca694@gmail.com>
2023-06-17 01:06:17 +0000

    Added templates to thrall app

--- thrall/views.py
+++ thrall/views.py
@@ -4,3 +4,8 @@ from django.shortcuts import render
 # Create your views here.
 def get_thrall_list(request):
     return render(request, 'thrall/thrall_list.html')
+
+def index(request):
+    """ A view to return the index page """
+
+    return render(request, 'thrall/index.html')
