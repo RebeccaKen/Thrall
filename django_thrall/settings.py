8e6af5715b3a66d2db81ea87eb3b96429e4f5e8b
RebeccaKen <kennedyrebecca694@gmail.com>
2023-06-21 23:27:36 +0000

    Add procfile to project

--- django_thrall/settings.py
+++ django_thrall/settings.py
@@ -114,6 +114,11 @@ LOGIN_REDIRECT_URL = '/'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+    }
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
