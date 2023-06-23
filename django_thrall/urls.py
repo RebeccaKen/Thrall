23d4201f418ec1537e9ac56b1c3aa9e6681017c8
RebeccaKen <kennedyrebecca694@gmail.com>
2023-06-23 19:28:10 +0000

    Add block content to base.html

--- django_thrall/urls.py
+++ django_thrall/urls.py
@@ -8,5 +8,5 @@ urlpatterns = [
     path('accounts/', include('allauth.urls')),
     path('', include('thrall.urls')),
     path('summernote/', include('django_summernote.urls')),
-    path('', include('blog.urls'), name='blog_urls')
+    path('blog/', include('blog.urls'))
 ]
