5b7a668bac76a0bd95a9cec6732c39ee1b7cb384
RebeccaKen <kennedyrebecca694@gmail.com>
2023-06-26 16:25:56 +0000

    Remove thrall.list to attempt solution of recursion error

--- thrall/views.py
+++ thrall/views.py
@@ -1,11 +1,6 @@
 from django.shortcuts import render
 
 
-# Create your views here.
-def get_thrall_list(request):
-    return render(request, 'thrall/thrall_list.html')
-
-
 def index(request):
     """ A view to return the index page """
 
