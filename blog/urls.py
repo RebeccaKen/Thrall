b16b4493dc9ed6206d648e9481e96ebb6908c994
RebeccaKen <kennedyrebecca694@gmail.com>
2023-06-27 17:53:09 +0000

    Attempt fix on blog post issue

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,8 @@
 from . import views
 from django.urls import path
 
+
 urlpatterns = [
-    path("", views.PostList.as_view(), name="post_list"),
-    path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
+    path('PostList', views.post_list, name='post_list'),
+    path('PostDetails/', views.PostDetail.as_view(), name="post_detail"),
 ]
