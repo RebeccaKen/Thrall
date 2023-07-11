ab94c1dc8a17ee101ba820fc6b9617b8043b09fb
RebeccaKen <kennedyrebecca694@gmail.com>
2023-07-11 00:57:36 +0000

    Add new styling and links to footer

--- contact/admin.py
+++ contact/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Contact
 
-# Register your models here.
+@admin.register(Contact)
+class ContactAdmin(admin.ModelAdmin):
+
+    list_display = ('name', 'email', 'date', 'rating')
+    list_filter = ('date', 'rating')
+    search_fields = ('name', 'email', 'comments')
+    actions = ['approve_feedback']
+
+    def approve_Comments(self, request, queryset):
+        queryset.update(approved=True)
+
