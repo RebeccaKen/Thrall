6a3efcd7b5010ac23326e1a8311e689148bc1c23
RebeccaKen <kennedyrebecca694@gmail.com>
2023-07-12 20:55:55 +0000

    Update contact_view to validate the contact form

--- contact/views.py
+++ contact/views.py
@@ -1,8 +1,17 @@
 from django.shortcuts import render
 from .models import Contact
 from .forms import ContactForm
+from django.shortcuts import render, redirect
 
 
 def contact_view(request):
-    form = ContactForm()
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            contact = form.save()
+            messages.success(request, 'Your contact form have been sent.')
+            return redirect('thrall')
+    else:
+        form = ContactForm()
+    
     return render(request, 'contact/contact_form.html', {'form': form})
