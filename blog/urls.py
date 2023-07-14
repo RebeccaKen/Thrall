2216132f76edcb79ec7cfd91756f1cb9b7086504
RebeccaKen <kennedyrebecca694@gmail.com>
2023-07-14 02:14:46 +0000

    Add the funtionality to comment on posts

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,7 @@
 from . import views
 from django.urls import path, reverse
 from django.urls import path
-from .views import PostList, PostDetail
+from views import PostDetail, PostLike, PostList
 
 app_name = 'blog'
 
@@ -9,4 +9,5 @@ app_name = 'blog'
 urlpatterns = [
     path('', PostList.as_view(), name='post_list'),
     path('post/<slug:slug>/', PostDetail.as_view(), name='post_detail'),
+    path('like/<slug:slug>', views.PostLike.as_view(), name='post_like'),
 ]
