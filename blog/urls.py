828769deee01aad908083a7d743e64d0eea5c1d0
RebeccaKen <kennedyrebecca694@gmail.com>
2023-07-14 18:03:19 +0000

    Register namespace in blog app urls

--- blog/urls.py
+++ blog/urls.py
@@ -1,13 +1,10 @@
 from . import views
-from django.urls import path, reverse
 from django.urls import path
-from views import PostDetail, PostLike, PostList
 
 app_name = 'blog'
 
-
 urlpatterns = [
-    path('', PostList.as_view(), name='post_list'),
-    path('post/<slug:slug>/', PostDetail.as_view(), name='post_detail'),
+    path('', views.PostList.as_view(), name='post_list'),
+    path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
     path('like/<slug:slug>', views.PostLike.as_view(), name='post_like'),
 ]
