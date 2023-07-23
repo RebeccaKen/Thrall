59b736142580cb1f61a38cabf398a33f6d3e8f26
RebeccaKen <kennedyrebecca694@gmail.com>
2023-07-23 17:04:37 +0000

    Remove commented out code from static files

--- wishlist/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Wishlist
-
-class WishlistAdmin(admin.ModelAdmin):
-    list_display = (
-        'name',
-        'description',
-    )
-admin.site.register(Wishlist)
