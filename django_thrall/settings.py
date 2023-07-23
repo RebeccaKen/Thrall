399e2bdd64a0738408062c3b9969a006932b4348
RebeccaKen <kennedyrebecca694@gmail.com>
2023-07-23 00:44:06 +0000

    Removed secret key and set debug

--- django_thrall/settings.py
+++ django_thrall/settings.py
@@ -26,10 +26,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'thewitcher'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 
 ALLOWED_HOSTS = ['thrall-e-commerce-website-caefc95c2f76.herokuapp.com', 'localhost']
