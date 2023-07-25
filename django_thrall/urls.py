6193d96d0cbc8d03e1552d83ee18391226db7dd7
RebeccaKen <kennedyrebecca694@gmail.com>
2023-07-25 00:39:03 +0000

    Add logic for 404 error handling

--- django_thrall/urls.py
+++ django_thrall/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+import thrall.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -15,3 +16,6 @@ urlpatterns = [
     path('profile/', include('profiles.urls')),
     path('checkout/', include('checkout.urls')),
     ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+handler404 = 'thrall.views.handler404'
+handler500 = 'thrall.views.handler500'
--- thrall/views.py
+++ thrall/views.py
@@ -1,5 +1,20 @@
 from django.shortcuts import render
+from django.template import RequestContext
 
 
 def index(request):
     return render(request, 'thrall/index.html')
+
+
+def handler404(request, *args, **argv):
+    response = render_to_response('404.html', {},
+                                  context_instance=RequestContext(request))
+    response.status_code = 404
+    return response
+
+
+def handler500(request, *args, **argv):
+    response = render_to_response('500.html', {},
+                                  context_instance=RequestContext(request))
+    response.status_code = 500
+    return response
